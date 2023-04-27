d55514584af204d68487cde51868aa6711418294
KaninWithRice <mishalnorona014@gmail.com>
2023-04-28 03:20:11 +0800

    Introduction

---
+++ SandC_Calculator.py
@@ -0,0 +1,11 @@
+# Mohammad Mishal S. Noroña | BSCPE 1-5 | Porblem 4 Square and Cube
+# Add introduction
+# Ask user for numbers of input
+# Ask user to enter numbers
+# Create a text file for user's input
+# Create a Formula for Separating Even and Odd
+# Add Loading Time
+# Create a double.txt file
+# Print Squared Itegers
+# Create a triple.txt file  
+# Print Cubed Itegers 
