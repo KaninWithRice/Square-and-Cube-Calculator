cc36e6da11090329425e97952bf146f5e5f1481d
KaninWithRice <mishalnorona014@gmail.com>
2023-04-28 03:34:21 +0800

    Enter a Integer

--- SandC_Calculator.py
+++ SandC_Calculator.py
@@ -9,6 +9,11 @@ num_input = int(input("Enter how many you want to input: "))
 user_input_list = open("integers.txt","w")
 
 # Ask user to enter numbers
+numbers = []
+for i in range(1, 1 + num_input):
+    user_input = (input("Enter a Integer: \t"))
+    numbers.append(user_input)
+
 # Create a text file for Integers
 # Create a Formula for Separating Even and Odd
 # Add Loading Time
