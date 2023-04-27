1c7b6cce7ef6b11a631640b1e4e399acd8aa6120
KaninWithRice <mishalnorona014@gmail.com>
2023-04-28 03:33:21 +0800

    Ask for Numbers of Input

--- SandC_Calculator.py
+++ SandC_Calculator.py
@@ -5,6 +5,9 @@ print("WELCOME TO THE SQUARE AND CUBE CALCULATOR".center(40," ") )
 print("By: Mishal Noroña".center(40," ") )
 
 # Ask user for numbers of input
+num_input = int(input("Enter how many you want to input: "))
+user_input_list = open("integers.txt","w")
+
 # Ask user to enter numbers
 # Create a text file for Integers
 # Create a Formula for Separating Even and Odd
